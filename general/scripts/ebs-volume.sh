00978fdc6cd2499462c6dc9b28e63dbc4b5b6e51
Sean Newman <sxn6296@rit.edu>
2019-10-12 16:57:03 -0400

    Can't use normal bash variables anymore apparently

--- general/scripts/ebs-volume.sh
+++ general/scripts/ebs-volume.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 mkfs -t ext4 ${device}
 mkdir ${mountpoint}
-UUID=$(lsblk -no UUID ${device})
 cat >> /etc/fstab << EOF
-UUID=${UUID} ${mountpoint} ext4 defaults,nofail 0 2
+UUID=$(lsblk -no UUID ${device}) ${mountpoint} ext4 defaults,nofail 0 2
 EOF
 mount -a
