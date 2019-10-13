9e6fa260221ba2820f4e855381b9afc1cdf978d7
Sean Newman <sxn6296@rit.edu>
2019-10-13 04:51:17 -0400

    Use staging like a wyld man

--- general/main.tf
+++ general/main.tf
@@ -13,5 +13,5 @@ provider "aws" {
 }
 
 provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
 }
